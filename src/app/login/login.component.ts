3d07f1a8ae6dcf44d95bafe429322914e7257644
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-07-22 22:26:37 +0200

    validación formularios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -15,23 +20,28 @@ import { RouterOutlet } from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
   errorMessage = '';
+  submitted = false;
 
   constructor(private authService: AuthService, private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   isControlInvalid(controlName: string): boolean {
     const control = this.loginForm.get(controlName);
-    return control ? control.invalid && (control.dirty || control.touched) : false;
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
   }
 
   login(): void {
+    this.submitted = true;
 
     if (this.loginForm.invalid) {
-      this.errorMessage = 'Please fill out all fields correctly.';
+      this.errorMessage = 'Please fill out the form correctly.';
+      this.markAllFieldsAsTouched(); // Marca todos los campos como tocados
       return;
     }
 
@@ -41,7 +51,13 @@ export class LoginComponent {
       },
       error: (err) => {
         alert(err.error?.error || 'Invalid credentials');
-      }
+      },
+    });
+  }
+
+  markAllFieldsAsTouched() {
+    Object.values(this.loginForm.controls).forEach((control) => {
+      control.markAsTouched();
     });
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, AbstractControlOptions } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service'; 
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
@@ -21,37 +23,46 @@ export class RegisterComponent implements OnInit {
     private authService: AuthService,
     private router: Router
   ) {
-    // Inicialización del formulario reactivo
+   
+    const formOptions: AbstractControlOptions = {
+      validators: this.checkPasswords
+    };
+
     this.registerForm = this.formBuilder.group({
       fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
-    }, { validator: this.checkPasswords });
+      confirmPassword: ['', [Validators.required]]
+    }, formOptions);
   }
 
   ngOnInit(): void {}
 
 
-  checkPasswords(group: FormGroup) {
+  checkPasswords(group: AbstractControl): ValidationErrors | null {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { mismatch: true };
   }
 
+
   // Control 
   isControlInvalid(controlName: string): boolean {
     const control = this.registerForm.get(controlName);
-    return control ? control.invalid && (control.dirty || control.touched) : false;
+    return control ? control.invalid && (control.dirty || control.touched || this.submitted) : false;
   }
 
 
   register() {
+    this.submitted = true;
+
     if (this.registerForm.invalid) {
-      this.errorMessage = 'Please fill out all fields correctly.';
+      this.errorMessage = 'Please fill out the form correctly.';
+      this.markAllFieldsAsTouched(); // Marca todos los campos como tocados
       return;
     }
 
+    
     this.authService.register(this.registerForm.value).subscribe({
       next: () => {
    
@@ -69,4 +80,16 @@ export class RegisterComponent implements OnInit {
       }
     });
   }
-}
+
+
+
+    markAllFieldsAsTouched() {
+      Object.values(this.registerForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+
+  }
+
+
+
