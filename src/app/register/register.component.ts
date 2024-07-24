e45702996629c6692bcd12205e5a11d598fe5143
SaraAOrtega <149887883+SaraAOrtega@users.noreply.github.com>
2024-07-24 12:59:30 +0200

    cambios en register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, AbstractControlOptions } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service'; 
 import { ReactiveFormsModule } from '@angular/forms';
@@ -24,16 +24,12 @@ export class RegisterComponent implements OnInit {
     private router: Router
   ) {
    
-    const formOptions: AbstractControlOptions = {
-      validators: this.checkPasswords
-    };
-
     this.registerForm = this.formBuilder.group({
       fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]]
-    }, formOptions);
+    }, { validators: this.checkPasswords });
   }
 
   ngOnInit(): void {}
@@ -42,23 +38,29 @@ export class RegisterComponent implements OnInit {
   checkPasswords(group: AbstractControl): ValidationErrors | null {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
-    return password === confirmPassword ? null : { mismatch: true };
+    if (password !== confirmPassword) {
+      group.get('confirmPassword')?.setErrors({ mismatch: true });
+      return { mismatch: true };
+    } else {
+      group.get('confirmPassword')?.setErrors(null);
+      return null;
+    }
   }
-
-
-  // Control 
+  
   isControlInvalid(controlName: string): boolean {
     const control = this.registerForm.get(controlName);
-    return control ? control.invalid && (control.dirty || control.touched || this.submitted) : false;
+    if (!control) return false;
+    return control.invalid && (control.dirty || control.touched || this.submitted);
+
+  
   }
-
-
+  
   register() {
     this.submitted = true;
 
     if (this.registerForm.invalid) {
       this.errorMessage = 'Please fill out the form correctly.';
-      this.markAllFieldsAsTouched(); // Marca todos los campos como tocados
+      this.markAllFieldsAsTouched(); 
       return;
     }
 
